21fc31007e3ba02082d8a41c42a9ccf5c287f004
WhaleKun <40340321+whalejun@users.noreply.github.com>
2026-04-06 19:04:09 +0900

    必要内容导入

--- components/NotionPage.js
+++ components/NotionPage.js
@@ -4,7 +4,7 @@ import { isBrowser, loadExternalResource } from '@/lib/utils'
 import mediumZoom from '@fisch0920/medium-zoom'
 import 'katex/dist/katex.min.css'
 import dynamic from 'next/dynamic'
-import { useEffect, useRef } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { NotionRenderer } from 'react-notion-x'
 
 /**

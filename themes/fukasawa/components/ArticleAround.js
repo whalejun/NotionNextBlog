9d3c0607d30c7f9abf4819c77c561293723f79a0
WhaleKun <40340321+whalejun@users.noreply.github.com>
2026-04-06 16:16:13 +0900

    去除上下篇文章显示

--- themes/fukasawa/components/ArticleAround.js
+++ themes/fukasawa/components/ArticleAround.js
@@ -10,23 +10,24 @@ export default function ArticleAround ({ prev, next }) {
     return <></>
   }
   return (
-    <section className='text-gray-800 h-28 flex items-center justify-between space-x-5 my-4'>
-      <SmartLink
-        href={`/${prev.slug}`}
-        passHref
-        className='text-sm cursor-pointer justify-center items-center flex w-full h-full bg-white bg-opacity-40 hover:bg-hexo-black-gray dark:bg-hexo-black-gray dark:text-gray-200 hover:text-white duration-300'>
+    // 去除下上一篇，下一篇文章
+    // <section className='text-gray-800 h-28 flex items-center justify-between space-x-5 my-4'>
+    //   <SmartLink
+    //     href={`/${prev.slug}`}
+    //     passHref
+    //     className='text-sm cursor-pointer justify-center items-center flex w-full h-full bg-white bg-opacity-40 hover:bg-hexo-black-gray dark:bg-hexo-black-gray dark:text-gray-200 hover:text-white duration-300'>
 
-        <i className='mr-1 fas fa-angle-double-left' />{prev.title}
+    //     <i className='mr-1 fas fa-angle-double-left' />{prev.title}
 
-      </SmartLink>
-      <SmartLink
-        href={`/${next.slug}`}
-        passHref
-        className='text-sm  cursor-pointer justify-center items-center flex w-full h-full bg-white bg-opacity-40 hover:bg-hexo-black-gray dark:bg-hexo-black-gray dark:text-gray-200 hover:text-white duration-300'>
-        {next.title}
-        <i className='ml-1 my-1 fas fa-angle-double-right' />
+    //   </SmartLink>
+    //   <SmartLink
+    //     href={`/${next.slug}`}
+    //     passHref
+    //     className='text-sm  cursor-pointer justify-center items-center flex w-full h-full bg-white bg-opacity-40 hover:bg-hexo-black-gray dark:bg-hexo-black-gray dark:text-gray-200 hover:text-white duration-300'>
+    //     {next.title}
+    //     <i className='ml-1 my-1 fas fa-angle-double-right' />
 
-      </SmartLink>
-    </section>
+    //   </SmartLink>
+    // </section>
   );
 }
